65df8a61fcf7c3b317150385e8aa37815a245960
Elena Kulakova <kulakovaelena15@gmail.com>
2019-11-08 10:58:09 +0100

    Setting the UIView. Implementing action for showing the questions.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let quiz = [ ["Bear has two legs.", "True" ],
+                 ["2 + 2 = 3", "False"],
+                 ["My son name is Kristofer.", "True"]
+    ]
+    
+    var questionNumber = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+        
+        
     }
-
-
+    
+    @IBAction func answerPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle// True/False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right")
+        } else {
+            print("Not right")
+        }
+        
+        if questionNumber + 1 <= 2 { // quiz.count
+            questionNumber += 1
+            updateUI()
+        } else {
+            questionNumber = 0
+        }
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
 }
 
